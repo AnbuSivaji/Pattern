db4856b000a55d56ed8b0048bf38f6f43895cf94
AnbuSivaji <anbusoft57@gmail.com>
2025-04-13 17:08:30 +0530

    Pattern method

---
+++ Pattern2.java
@@ -0,0 +1,14 @@
+public class Pattern2 {
+    public static void main(String[] args) {
+       int row=5;
+        
+       
+        for(int i=row;i>=0;i--){
+            for(int j=1;j<=i;j++){
+            System.out.print(j);//you want * pattern using *
+        }
+        System.out.println(" ");
+
+    }
+}
+}
---
+++ Pyramid_Pattern.java
@@ -0,0 +1,17 @@
+public class Pyramid_Pattern {
+    public static void main(String[] args) {
+        int row=5;
+        for(int i=1;i<=row;i++){
+            for(int j=i;j<row;j++){
+               
+                System.out.print(" ");
+            }
+            for(int k=1;k<=(2*i-1);k++){
+                System.out.print(k);
+            }
+            System.out.println(" ");
+        
+    }
+    
+}
+}
